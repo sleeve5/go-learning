7213d4ec94ac0d2f9de27e98f1d1c98324c20630
sleeve5 <1244595156@qq.com>
2022-05-07 13:34:46 +0800

    commit

--- day1/guessing-game/main.go
+++ day1/guessing-game/main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
+	// 生成随机数
 	maxNum := 100
 	rand.Seed(time.Now().UnixNano())
 	result := rand.Intn(maxNum)
 	fmt.Printf("The result is %v.\n", result)
+
+	// 读取用户输入
+	fmt.Println("Please input your guess:")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("An error occurred while reading input. Please try again.", err)
+		return
+	}
+	// windows 与 linux不同，末尾加上\r\n作为换行符
+	input = strings.TrimSuffix(input, "\r\n")
+
+	// 输入转化为数字
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Invalid input. Please input an integer value.", err)
+		return
+	}
+	fmt.Printf("Your guess is %v.\n", number)
+
 }
--- hello.go
+++ hello.go
@@ -3,9 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	var age = 19
-	var birthday = "20020813"
-	var name = "Yin Xinyu"
-	var result = fmt.Sprintf("\nname: %s, \nbirthday: %s, \nage %d. \n", name, birthday, age)
-	fmt.Println(result)
+	fmt.Println("hello go")
 }
