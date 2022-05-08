59511a8477fed2046b08d512784964b38b3f3d61
sleeve5 <1244595156@qq.com>
2022-05-08 20:52:49 +0800

    commit

---
+++ day1/guess/v2/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// 设置最大值
+	maxNum := 100
+	rand.Seed(time.Now().UnixNano())
+	result := rand.Intn(maxNum)
+	for {
+		// 利用scanf输入
+		fmt.Println("Please input your guess:")
+		var number int
+		_, err := fmt.Scanf("%d", &number)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		// 判断部分
+		if result < number {
+			fmt.Println("Your guess is bigger than the result!")
+		} else if result > number {
+			fmt.Println("Your guess is smaller than the result!")
+		} else if result == number {
+			fmt.Println("Correct!")
+			return
+		}
+	}
+}
